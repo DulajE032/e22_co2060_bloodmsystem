b99bf070ab62f43c05d5232e971a5ae5141acb0d
DulajE032 <e22032@eng.pdn.ac.lk>
2026-05-01 14:20:34 +0530

    fff

--- backEnd/main/apps/donor/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .donorDetails import DonorDetails
-from .donorAlert import DonorAlert
-from .donationHistory import DonationHistory
-from .bloodCamp import BloodCamp
-from .campRegistration import CampRegistration
-
-__all__ = ["DonorDetails", "DonorAlert", "DonationHistory", "BloodCamp", "CampRegistration"]
